929f4cff128c513bb62ceadfb4f346879e951941
aiman1717a <aiman1717a@gmail.com>
2020-02-17 23:02:54 +0500

    vv

--- video.js
+++ video.js
@@ -1,15 +1,34 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.querySelector("#videoElement");
 
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-            })
-            .catch(function (err0r) {
-                console.log("Something went wrong!");
-            });
+    const video = document.querySelector("#videoElement");
+    Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+    ]).then(startVideo);
+
+    function startVideo() {
+        navigator.getUserMedia(
+            {video: {}},
+            stream => video.srcObject = stream,
+            error => console.log(error)
+        )
     }
+    video.addEventListener('play', function() {
+        console.log('playing');
+        const canvas = faceapi.createCanvasFromMedia(video);
+        document.body.append(canvas);
+        const displaySize = {width: video.width, height: video.height};
+        faceapi.matchDimensions(canvas, displaySize);
+        console.log('playing');
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video,
+                new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+            console.log(detections);
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+        }, 100)
+    });
 
-});
