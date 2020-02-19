0dd6d74307bac1cc081d66bf65dcc31708d1394c
aiman1717a <aiman1717a@gmail.com>
2020-02-19 12:23:20 +0500

    v1.0

--- video.js
+++ video.js
@@ -24,9 +24,10 @@
         var canvas = faceapi.createCanvasFromMedia(video);
         // var container = document.querySelector(".container");
         document.body.append(canvas);
-        const displaySize = {width: video.videoWidth, height: video.videoHeight};
-        faceapi.matchDimensions(canvas, displaySize);
         setInterval(async () =>  {
+            var container = document.getElementById("container");
+            var displaySize = {width: container.offsetWidth, height: container.offsetHeight};
+            faceapi.matchDimensions(canvas, displaySize);
             const detections = await faceapi.detectAllFaces(video,
                 new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
             console.log(detections);
@@ -34,8 +35,6 @@
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
             faceapi.draw.drawDetections(canvas, resizedDetections);
             faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+            // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         }, 100)
     });
-    // video.addEventListener('loadeddata', function() {
-    //
-    // });
